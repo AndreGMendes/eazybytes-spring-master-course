619ae83d3561e81d89de3f16e4ed5ddb120c1b6a
André Mendes <andregmendes@gmail.com>
2023-06-23 16:49:21 +0100

    Added my wip version of this Tutorial

---
+++ my-eazybytes-project/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,14 @@
+package com.example.beans;
+
+public class Vehicle {
+
+    private String name;
+
+    public String getVehicleName() {
+        return name;
+    }
+
+    public void setVehicleName (String name) {
+        this.name = name;
+    }
+}
